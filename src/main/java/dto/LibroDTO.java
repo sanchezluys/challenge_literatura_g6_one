2c98664f3c96d8e59b208c02e5b49b8c94b223a2
luys <sanchezluys@gmail.com>
2024-07-08 12:10:37 -0500

    se agrega jackson a dependencias

--- src/main/java/dto/LibroDTO.java
+++ src/main/java/dto/LibroDTO.java
@@ -1,14 +1,18 @@
 package dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record LibroDTO(
         Integer id,
-        String title,
-        String languages,
-        Integer download_count,
-        List<String> authors)
+        @JsonAlias("Titulo") String title,
+        @JsonAlias("Idiomas") String languages,
+        @JsonAlias("Descargas") Integer download_count,
+        @JsonAlias("Autores") List<String> authors
+    )
 {
 
 }
